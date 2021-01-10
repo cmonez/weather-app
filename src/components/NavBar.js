99d15a2a2278821a6431b6b821857beb4ce58ad1
Eric Zepeda <zepedaeric@gmail.com>
2021-01-10 00:30:07 -0800

    Add zipcode state to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -6,12 +7,23 @@ import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
 const NavBar = () => {
+  const [zipCode, setZipCode] = useState('');
+  const onChange = (event) => {
+    console.log('zipcode', event.target.value);
+    setZipCode(event.target.value);
+  };
+
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Form inline>
-          <FormControl type="text" placeholder="Zip Code" className="mr-sm-2" />
+          <FormControl
+            type="text"
+            placeholder="Zip Code"
+            className="mr-sm-2"
+            onChange={onChange}
+          />
           <Button variant="outline-success">Search</Button>
         </Form>
       </Navbar.Collapse>
