415eb6796eab7795aac533e4cec3d8eec2e0238f
Eric Zepeda <zepedaeric@gmail.com>
2021-01-09 22:09:01 -0800

    Import bootstrap stylesheet, create basic nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <NavBar></NavBar>
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+const NavBar = () => {
+  return (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+      <Form inline>
+        <FormControl type="text" placeholder="Zip Code" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+  );
+};
+
+export default NavBar;
