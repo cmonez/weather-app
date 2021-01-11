68436f83b79cd6e31a7ef37545fc6a359e598043
Eric Zepeda <zepedaeric@gmail.com>
2021-01-10 16:02:34 -0800

    Grab fahrenheit

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const getWeatherInformationByZipcode = async (zipCode) => {
   let response = await axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=655dfc390726be35679ee1f171b45301`
+    `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=655dfc390726be35679ee1f171b45301&units=imperial&precipitation`
   );
+  return response;
 };
+
+export default getWeatherInformationByZipcode;
